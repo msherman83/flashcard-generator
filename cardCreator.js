278caf2a2eccb0270f328508126de651a6799f3a
msherman83 <msherman83@gmail.com>
2017-12-04 20:47:49 -0500

    added clozecard and else to cardCreator logic to create flashcards

--- cardCreator.js
+++ cardCreator.js
@@ -9,43 +9,94 @@ function CardCreator() {
 
 };
 
-// Create a "Prompt" with a series of questions.
-inquirer
-.prompt([
-  // Front of flash card.
-  {
-    type: "input",
-    message: "Type the question you want asked on the front of the flash card.",
-    name: "flashcardFront"
-  },
-  {
-    type: "confirm",
-    message: "Are you sure:",
-    name: "confirm",
-    default: true
-  },
-  // Back of flash card
-  {
-    type: "input",
-    message: "Type the answer you would like on the back of the flash card.",
-    name: "flashcardBack"
-  },
-  {
-    type: "confirm",
-    message: "Are you sure:",
-    name: "confirm",
-    default: true
-  },
-])
-.then(function(inquirerResponse) {
 
-  if (inquirerResponse.confirm) {
-    flashcardFront = inquirerResponse.flashcardFront
-    flashcardBack = inquirerResponse.flashcardBack
-    
-    console.log(flashcardFront + " , " + flashcardBack)
-  }
-  else {
-    console.log("its broke...");
-  }
-});
+
+// Create a "Prompt" with a series of questions.
+
+if (process.argv[2] === "basiccard") {
+
+    inquirer
+        .prompt([
+            // Front of flash card.
+            {
+                type: "input",
+                message: "Type the question you want asked on the front of the flash card.",
+                name: "flashcardFront"
+            },
+            {
+                type: "confirm",
+                message: "Are you sure:",
+                name: "confirm",
+                default: true
+            },
+            // Back of flash card
+            {
+                type: "input",
+                message: "Type the answer you would like on the back of the flash card.",
+                name: "flashcardBack"
+            },
+            {
+                type: "confirm",
+                message: "Are you sure:",
+                name: "confirm",
+                default: true
+            },
+        ])
+        .then(function (inquirerResponse) {
+
+            if (inquirerResponse.confirm) {
+                flashcardFront = inquirerResponse.flashcardFront
+                flashcardBack = inquirerResponse.flashcardBack
+
+                console.log(flashcardFront + " , " + flashcardBack)
+            }
+            else {
+                console.log("its broke...");
+            }
+        });
+
+} else if (process.argv[2] === "clozecard") {
+
+    inquirer
+        .prompt([
+            // Full text of flash card.
+            {
+                type: "input",
+                message: "What would you like the full text of the card to read?",
+                name: "fulltext"
+            },
+            {
+                type: "confirm",
+                message: "Are you sure:",
+                name: "confirm",
+                default: true
+            },
+            // Text of flash card to be left out.
+            {
+                type: "input",
+                message: "What word(s) would you like to leave out?",
+                name: "cloze"
+            },
+            {
+                type: "confirm",
+                message: "Are you sure:",
+                name: "confirm",
+                default: true
+            },
+        ])
+        .then(function (inquirerResponse) {
+
+            if (inquirerResponse.confirm) {
+                fulltext = inquirerResponse.fulltext
+                cloze = inquirerResponse.cloze
+
+                console.log(fulltext + " , " + cloze)
+            }
+            else {
+                console.log("its broke...");
+            }
+        });
+
+} else {
+    console.log("Enter a valid option.")
+}
